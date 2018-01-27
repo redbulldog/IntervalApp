8bc327a812b3874ffe920b2b0f3f2bf1b2be27b1
Unknown <kovacs.zoltan195@gmail.com>
2018-01-27 18:33:20 +0100

    .

--- app/src/main/java/com/example/pcgur/intervalapp/Counter.java
+++ app/src/main/java/com/example/pcgur/intervalapp/Counter.java
@@ -1,5 +1,7 @@
 package com.example.pcgur.intervalapp;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,13 +12,15 @@ import butterknife.ButterKnife;
 
 public class Counter extends AppCompatActivity {
     @BindView(R.id.szamlalo) TextView szamlalo;
+    Integer korok, breaks, interval_seconds, interval_minutes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_counter);
         ButterKnife.bind(this);
-        new CountDownTimer(30000, 1000) {
+        sharedToVariable();
+        new CountDownTimer(interval_minutes*60000+interval_seconds*1000, 1000) {
 
             public void onTick(long millisUntilFinished) {
                 szamlalo.setText("seconds remaining: " + millisUntilFinished / 1000);
@@ -26,5 +30,12 @@ public class Counter extends AppCompatActivity {
                 szamlalo.setText("done!");
             }
         }.start();
+    }
+    public void sharedToVariable(){
+        SharedPreferences sharedPreferences = getSharedPreferences("Interval", Context.MODE_PRIVATE);
+        this.korok = sharedPreferences.getInt("korok", 0);
+        this.breaks = sharedPreferences.getInt("breaks", 0);
+        this.interval_seconds = sharedPreferences.getInt("interval_seconds", 0);
+        this.interval_minutes = sharedPreferences.getInt("interval_minutes", 0);
     }
 }
--- app/src/main/java/com/example/pcgur/intervalapp/MainActivity.java
+++ app/src/main/java/com/example/pcgur/intervalapp/MainActivity.java
@@ -1,5 +1,13 @@
 package com.example.pcgur.intervalapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.preference.RingtonePreference;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -16,7 +24,6 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.addnew) FloatingActionButton addnew;
     private AlertDialog.Builder felh_nev_input;
-    private PostsDatabaseHelper mydb;
 
 
     @Override
@@ -24,35 +31,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        final View mView = getLayoutInflater().inflate(R.layout.addnewitem, null);
+        final EditText edt_intervallum_minutes = (EditText) mView.findViewById(R.id.edt_intervallum_minutes);
+        final EditText edt_intervallum_seconds = (EditText) mView.findViewById(R.id.edt_intervallum_seconds);
+        final EditText edt_korok = (EditText) mView.findViewById(R.id.edt_korok);
+        final EditText edt_breaks = (EditText) mView.findViewById(R.id.edt_breaks);
+        final EditText edt_name = (EditText) mView.findViewById(R.id.edt_name);
+        final CheckBox chk_mentes = (CheckBox) mView.findViewById(R.id.chk_mentes);
+        final Button btn_sound = (Button) mView.findViewById(R.id.btn_sound);
+        final Button btn_start = (Button) mView.findViewById(R.id.btn_start);
+        MediaPlayer mp;
 
         addnew.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 felh_nev_input = new AlertDialog.Builder(MainActivity.this);
-                View mView = getLayoutInflater().inflate(R.layout.addnewitem, null);
-                final EditText edt_intervallum = (EditText) mView.findViewById(R.id.edt_intervallum);
-                final EditText edt_korok = (EditText) mView.findViewById(R.id.edt_korok);
-                final EditText edt_breaks = (EditText) mView.findViewById(R.id.edt_breaks);
-                final EditText edt_name = (EditText) mView.findViewById(R.id.edt_name);
-                final CheckBox chk_mentes = (CheckBox) mView.findViewById(R.id.chk_mentes);
-                Button btn_start = (Button) mView.findViewById(R.id.btn_start);
+                btn_sound.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        /*Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
+                        intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE, RingtoneManager.TYPE_NOTIFICATION);
+                        intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE, "Válassz hangot!");
+                        intent.putExtra(RingtoneManager.EXTRA_RINGTONE_EXISTING_URI, (Uri)null);
+                        startActivity(intent);
+                        try {
+                            Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                            Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+                            r.play();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }*/
+
+                    }
+                });
+
                 btn_start.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         if (!chk_mentes.isChecked()){
-                            Integer interval = Integer.parseInt(edt_intervallum.getText().toString());
+                            Integer interval_minutes = Integer.parseInt(edt_intervallum_minutes.getText().toString());
+                            Integer interval_seconds = Integer.parseInt(edt_intervallum_seconds.getText().toString());
                             Integer korok = Integer.parseInt(edt_korok.getText().toString());
                             Integer breaks = Integer.parseInt(edt_breaks.getText().toString());
                             String name = edt_name.getText().toString();
-
-                            try {
-                            Boolean resoult = mydb.adatRogzites(interval, korok, breaks, name);
-                                Toast.makeText(MainActivity.this, "Adatrögzítés sikeres!", Toast.LENGTH_SHORT).show();
-
-                            }catch(Exception e){
-                                Toast.makeText(MainActivity.this, "Adatrögzítés sikertelen!", Toast.LENGTH_SHORT).show();
-                            }
-
+                            SharedPreferences sharedPreferences = getSharedPreferences("Interval", Context.MODE_PRIVATE);
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.putString("name", name.toString());
+                            editor.putInt("korok", korok);
+                            editor.putInt("breaks", breaks);
+                            editor.putInt("interval_minutes", interval_minutes);
+                            editor.putInt("interval_seconds", interval_seconds);
+                            editor.commit();
+                            Toast.makeText(MainActivity.this, "Siker", Toast.LENGTH_SHORT).show();
+                            Intent start = new Intent(MainActivity.this, Counter.class);
+                            startActivity(start);
                         }
                     }
                 });
@@ -66,7 +98,5 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-    private void adatFelvetel(){
 
-    }
 }
